bca8104d958b44b972ca4b6949c5eea51369ed0e
Robert Vock <robert.vock@4wdmedia.de>
2020-04-28 13:45:49 +0200

    Added svg-inliner for Smarty

---
+++ Resources/plugins/function.svg.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types = 1);
+
+use Vierwd\SvgInliner\SvgInliner;
+
+function smarty_function_svg(array $params, Smarty_Internal_Template $smarty): string {
+	static $svgInliner;
+
+	if (!$svgInliner) {
+		$svgInliner = new SvgInliner(['excludeFromConcatenation' => true]);
+	}
+	$params = $params + [
+		'output' => false,
+		'width' => false,
+		'height' => false,
+		'src' => false,
+		'value' => false,
+		'class' => false,
+	];
+
+	if ($params['output']) {
+		return $svgInliner->renderFullSVG();
+	}
+
+	if ($params['value']) {
+		return $svgInliner->renderSVG($params['value'], $params);
+	}
+
+	if ($params['src']) {
+		return $svgInliner->renderSVGFile($params['src'], $params);
+	}
+
+	throw new Exception('Unknown SVG. Missing src or value attribute', 1588074195);
+}
